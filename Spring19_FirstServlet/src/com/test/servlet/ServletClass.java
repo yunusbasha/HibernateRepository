81420c7f50b70d700428b0f20ba86fd2fb704cdb
Yunus Basha <yunusbasha1988@gmail.com>
2015-12-13 17:58:47 -0500

    Spring 19: First Servlet

---
+++ Spring19_FirstServlet/src/com/test/servlet/ServletClass.java
@@ -0,0 +1,28 @@
+package com.test.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletClass extends HttpServlet{
+
+	/* From web.xml it execute all methods of the class*/
+	public ServletClass()
+	{
+		System.out.println("Constructor creates an object......");
+	}
+	
+	public void init()
+	{
+		System.out.println("init() method is called......");
+	}
+	
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		System.out.println("service() method is called.....");
+	}
+	
+	
+}
